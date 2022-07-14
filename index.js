5c0a4c3eab7144f5fec6921d5d4b751218b8f6b6
Enrico Biancotto <enrico.biancotto@studenti.unipd.it>
2022-07-14 18:57:12 +0200

    Add ex 3.13-3.18

--- index.js
+++ index.js
@@ -1,53 +1,56 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const Person = require('./models/person')
 
 const app = express()
 app.use(express.json())
+app.use (cors())
 app.use(express.static('build'))
 
-// step 8
 morgan.token('body', (request, response) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'))
 
-// step 1
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons=> {
         response.json(persons)
     })
 })
 
-// step 2
 app.get('/info', (request, response) => {
-    response.send(`<div><p>Phonebook has info for ${persons.length} people</p><p> ${new Date().toString()}</p></div>`)
+    Person.count({}, (err, count) => {
+        if (err) {
+            console.error("Error counting entries")
+            return response.send(400).end()
+        }
+        response.send(`<div><p>Phonebook has info for ${count} people</p><p> ${new Date().toString()}</p></div>`)
+    })
 })
 
-// step 3
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person
         .findById(request.params.id)
         .then(person => {
+        if (person) {
             response.json(person)
+        } else {
+            response.status(404).end()
+        }
         })
+        .catch(error => next(error))
 })
 
-// step 4
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person
-        .deleteOne({ _id: request.params.id })
-        .then(person => {
+        .findByIdAndRemove(request.params.id)
+        .then(result => {
             response.status(204).end()
         })
-        .catch(err => {
-            response.json({
-                error: err
-            })
-        })
+        .catch(error => next(error))
         
 })
 
-// step 5
 app.post('/api/persons', (request, response) => {
     const body = request.body
     if (!body.name || !body.number) {
@@ -64,6 +67,41 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person
+        .findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+// middleware to handle non-existing routes
+const unkownEndpoint = (request, response) => {
+    response.status(404).send({
+        error: 'unkown endpoint'
+    })
+}
+app.use(unkownEndpoint)
+
+// custom error handler middleware
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformed id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
